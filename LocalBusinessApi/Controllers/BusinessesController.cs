0bb94a93a19bc5e9811ca376a1028891389fad13
Sisi Vieira <cicy886@gmail.com>
2021-11-19 18:25:53 -0800

    create Delete route for users to delete the buinesses

--- LocalBusinessApi/Controllers/BusinessesController.cs
+++ LocalBusinessApi/Controllers/BusinessesController.cs
@@ -77,6 +77,22 @@ namespace LocalBusinessApi.Controllers
       return CreatedAtAction(nameof(GetBusiness), new { id = business.BusinessId}, business);
     }
 
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBusiness(int id)
+    {
+      var business = await _db.Businesses.FindAsync(id);
+      if (business == null)
+      {
+        return NotFound();
+      }
+
+      _db.Businesses.Remove(business);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+      
+    }
+
     private bool BusinessExists(int id)
     {
       return _db.Businesses.Any(e => e.BusinessId == id);
