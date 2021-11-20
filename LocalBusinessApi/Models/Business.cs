eed672c3102d5cb446a456e70f557aa7313fa4ff
Sisi Vieira <cicy886@gmail.com>
2021-11-19 19:03:07 -0800

    add validations to properties in Business class

--- LocalBusinessApi/Models/Business.cs
+++ LocalBusinessApi/Models/Business.cs
@@ -1,12 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LocalBusinessApi.Models
 {
   public class Business
   {
     public int BusinessId { get; set; }
+    [Required]
+    [StringLength(50)]
     public string Name { get; set; }
+    [Required]
+    [StringLength(20)]
     public string Category { get; set; }
+    [Required]
     public string Description { get; set; }
+    [Required]
+    [Range(1, 5, ErrorMessage = "Rate must be between 1 and 5.")]
     public int Rate { get; set; }
+    [Required]
     public string Review { get; set; }
   }
 }
