304a3a3ab86b42b8b1f51a34511cc9ed6f6b7d95
Sisi Vieira <cicy886@gmail.com>
2021-11-19 17:08:43 -0800

    create business class and its properties

---
+++ LocalBusinessApi/Models/Business.cs
@@ -0,0 +1,12 @@
+namespace LocalBusiness.Models
+{
+  public class Business
+  {
+    public int BusinessId { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public string Description { get; set; }
+    public int Rate { get; set; }
+    public string Review { get; set; }
+  }
+}
