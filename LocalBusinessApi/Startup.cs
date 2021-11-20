9dea61daf11dc88f9195fc43066f68a906cdf454
Sisi Vieira <cicy886@gmail.com>
2021-11-19 16:08:35 -0800

    stopthe server to reroute traffic to the HTTPS port

--- LocalBusinessApi/Startup.cs
+++ LocalBusinessApi/Startup.cs
@@ -44,7 +44,7 @@ namespace LocalBusinessApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LocalBusinessApi v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
